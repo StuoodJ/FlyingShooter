4a5622c63e91bb47013864a2979fd9b8cb1b75f7
StuoodJ <Jessebetea@gmail.com>
2024-01-16 05:40:02 -0500

    turn it into a platformer

--- Raytracerinprogress.py
+++ Raytracerinprogress.py
@@ -2,27 +2,55 @@
 import pygame
 # pygame setup
 pygame.init()
-import functions
-from functions import *
+display = pygame.display
+screen = display.set_mode([1280, 720])
 clock = pygame.time.Clock()
 running = True
 dt = 0
-rayCol = (255, 0, 0)
-
+pC = (255, 0, 0)
+pX = 0
+toppY = 0
+pW = 50
+pH = 50
+floorX = 0
+floorY = 500
+floorW = 1280
+floorH = 220
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    Raytrace()
-    pygame.draw.rect(screen, rayCol, (rayX, rayY, rayW, rayH))
-    # flip() the display to put your work on screen
+    pygame.draw.rect(screen, pC, (pX, toppY, pW, pH))
+    floor = pygame.draw.rect(screen, (255, 255, 255), (floorX, floorY, floorW, floorH))
+    bottompY = toppY + pH
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        toppY -= 300 * dt
+    elif bottompY < floorY:
+        toppY += 30
+        
+    if keys[pygame.K_LEFT]:
+        pX -= 300 * dt
+    if keys[pygame.K_RIGHT]:
+        pX += 300 * dt
+
+
+    
+
+    if bottompY >= floorY:
+        toppY = 500 - pH
+
+
+
+
     display.flip()
+    
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
