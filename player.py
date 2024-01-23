d5045d63f6aa3bc41b895be12ee7fdc907d58040
StuoodJ <Jessebetea@gmail.com>
2024-01-23 02:38:48 -0500

    fit it

--- player.py
+++ player.py
@@ -1,19 +1,19 @@
 import pygame
 
 def Player(screen, playercolor, playerx, playery, bW, bH, dt, Xcon, Ycon):
-    playerrect = (playerx, playery, bW, bH)
     #playercode
     if Xcon > 0:
         #Right
         playerx += 350 * dt
-    elif Xcon < 0:
+    if Xcon < 0:
         #left
         playerx -= 350 * dt
-    elif Ycon < 0:
+    if Ycon < 0:
         #Up
         playery -= 350 * dt
-    elif Ycon > 0:
+    if Ycon > 0:
         #Down
         playery += 350 * dt
+    playerrect = (playerx, playery, bW, bH)
     #playerdraw
     pygame.draw.rect(screen, playercolor, playerrect)
