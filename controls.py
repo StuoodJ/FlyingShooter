f90395bcba0d665b7420da39ef1e3b857acdc39c
StuoodJ <Jessebetea@gmail.com>
2024-01-23 18:38:49 -0500

    fix it again

--- controls.py
+++ controls.py
@@ -5,12 +5,16 @@ def control(playerx, playery, dt, keys):
     if keys[pygame.K_LEFT]:
         #Lef
         playerx -= 350 * dt
+        return float([playerx])
     if keys[pygame.K_RIGHT]:
         #Right
         playerx += 350 * dt
+        return float([playerx])
     if keys[pygame.K_UP]:
         #Up
         playery -= 350 * dt
+        return float([playery])
     if keys[pygame.K_DOWN]:
         #Down
         playerx += 350 * dt
+        return float([playery])
