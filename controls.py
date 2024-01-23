31edca8ac774a4c962f112f1a820ceff17cd5521
StuoodJ <Jessebetea@gmail.com>
2024-01-23 02:26:08 -0500

    k

--- controls.py
+++ controls.py
@@ -4,17 +4,21 @@ Xcon = 0
 Ycon = 0
 def control():
     keys = pygame.key.get_pressed()
-
+#Xcontrols
     if keys[pygame.K_LEFT]:
         Xcon = -1
+        return Xcon
     elif keys[pygame.K_RIGHT]:
         Xcon = 1
+        return Xcon
     else:
         Xcon = 0
-
+#Ycontrols
     if keys[pygame.K_UP]:
         Ycon = -1
+        return Ycon
     elif keys[pygame.K_DOWN]:
         Ycon = 1
+        return Ycon
     else:
         Ycon = 0
