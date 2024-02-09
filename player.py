191d0adb2b67ea9926028adfdff4366912e58681
StuoodJ <Jessebetea@gmail.com>
2024-02-08 21:40:56 -0500

    duude

--- player.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-def Player(screen, playercolor, playerx, playery, bW, bH):
-    #playercode
-
-
-    playerrect = (playerx, playery, bW, bH)
-    #playerdraw
-    pygame.draw.rect(screen, playercolor, playerrect)
